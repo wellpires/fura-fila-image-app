fbaf1fd8a92c1d388690b612d31a2777f9e48f2d
Wellington PIres <wellington.goncalves@live.com>
2022-01-09 11:12:13 -0300

    First commit

---
+++ src/main/java/br/com/furafila/imageapp/FuraFilaImageAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.imageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaImageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaImageAppApplication.class, args);
+	}
+
+}
